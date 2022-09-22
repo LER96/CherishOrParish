1757ca6d6ed9cf1471d1f61beff4906c3105836b
LER96 <93140643+LER96@users.noreply.github.com>
2022-09-23 01:20:28 +0300

    new new

---
+++ solarPunk/Assets/Scripts/Dialog/Document.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName ="New Document", menuName ="Document")]
+public class Document : ScriptableObject
+{
+    [SerializeField] string documentTitle;
+    [SerializeField] string documentDescription; 
+}
