57f1f85272e0acea33275cb48483f2acbfec36be
layanmeta <layan.meta@gmail.com>
2022-09-23 02:21:56 +0300

    drawing

---
+++ solarPunk/Assets/LineCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class LineCollision : MonoBehaviour
+{
+    EdgeCollider2D collider;
+    LineRenderer line;
+
+    private void Start()
+    {
+        collider = this.GetComponent<EdgeCollider2D>();
+        line = this.GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        SetEdgeCollider(line);
+    }
+
+    void SetEdgeCollider(LineRenderer render)
+    {
+        List<Vector2> edges = new List<Vector2>();
+
+        for (int point = 0; point < line.positionCount; point++)
+        {
+            Vector3 lineRenderPoint = line.GetPosition(point++);
+            edges.Add(new Vector2(lineRenderPoint.x, lineRenderPoint.y));
+        }
+
+    }
+
+}
