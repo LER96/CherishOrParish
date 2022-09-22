c51737ec585538552ac320da870ad41798712f44
LER96 <93140643+LER96@users.noreply.github.com>
2022-09-23 02:20:12 +0300

    Document logic

---
+++ solarPunk/Assets/Scripts/Clock.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class Clock : GameManager
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
